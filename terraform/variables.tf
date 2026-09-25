022f556f149f851e1ff983b7d137b26e893b5649
asellai <asel.alibekkyzy@alatoo.edu.kg>
2026-09-25 07:46:13 +0600

    Add Terraform variables

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+  description = "Application name"
+  type        = string
+  default     = "demo-app"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
